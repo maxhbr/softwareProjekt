99c2bdc4612b673074a57dc064bff8dc27c5adc8
maximilianhuber <hubi@by-hubi.de>
2014-03-18 11:42:57 +0100

    weiter

--- src/Projekt/Core/FiniteField.hs
+++ src/Projekt/Core/FiniteField.hs
@@ -12,22 +12,8 @@ module Projekt.Core.FiniteField
   where
 import Projekt.Core.Polynomials
 
-{-
-import Prelude hiding (fromInteger, negate, (+), (-), (*), (/))
-
-class (Eq a) => FiniteField a where
-  zero, one          :: a
-  characteristik     :: Integer
-  fromInteger        :: Integer -> a
-  negate             :: a -> a
-  (+), (-), (*), (/) :: a -> a -> a
-  elems, units       :: [a]
- -}
-
 class (Eq a) => FiniteField a where
   zero, one    :: a
   elems, units :: [a]
-{-
- - TODO:
- - * Erweiterungen von Körpern
- -}
+
+
