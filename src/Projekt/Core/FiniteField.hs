f7ddb8c202a7619af1b71265272dd75c9316bdd5
hackenbergstefan <hackenberg.stefan@t-online.de>
2014-03-18 12:06:51 +0100

    .

--- src/Projekt/Core/FiniteField.hs
+++ src/Projekt/Core/FiniteField.hs
@@ -8,26 +8,18 @@
 --------------------------------------------------------------------------------
 
 module Projekt.Core.FiniteField
-  {-( FiniteField )-}
+  -- Data
+  (-- FFElem
+  )
   where
 import Projekt.Core.Polynomials
-
-{-
-import Prelude hiding (fromInteger, negate, (+), (-), (*), (/))
-
-class (Eq a) => FiniteField a where
-  zero, one          :: a
-  characteristik     :: Integer
-  fromInteger        :: Integer -> a
-  negate             :: a -> a
-  (+), (-), (*), (/) :: a -> a -> a
-  elems, units       :: [a]
- -}
 
 class (Eq a) => FiniteField a where
   zero, one    :: a
   elems, units :: [a]
-{-
- - TODO:
- - * Erweiterungen von Körpern
- -}
+
+
+--data FFElem a = FFElem (Polynom a) (Polynom a)
+
+
+
