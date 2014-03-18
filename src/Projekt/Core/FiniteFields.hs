6cff0ead56003952d730302932955b205d793bb6
maximilianhuber <hubi@by-hubi.de>
2014-03-18 15:17:33 +0100

    erwKpr

--- src/Projekt/Core/FiniteFields.hs
+++ src/Projekt/Core/FiniteFields.hs
@@ -11,6 +11,44 @@ module Projekt.Core.FiniteFields
   ) where
 import Projekt.Core.FiniteField
 import Projekt.Core.PrimeFields
+import Projekt.Core.Polynomials
+
+data FFElem a = FFElem (Polynom a) (Polynom a) | FFKonst a
+
+konstToElem :: (Num a, Eq a) => FFElem a -> Polynom a -> FFElem a
+konstToElem (FFKonst a) q              = FFElem (P [(0,a)]) q
+konstToElem (FFElem f p) q | p==q       = FFElem f p
+                           | otherwise = error "Not the same mod"
+
+instance Eq (FFElem a) where
+  x == y = undefined
+
+instance (Show a, Eq a) => Show (FFElem a) where
+  show (FFElem x y) = show x ++ " mod " ++ show y
+
+instance (Num a, Eq a, Fractional a) => Num (FFElem a) where
+  fromInteger i                           = FFKonst (fromInteger i)
+  (FFKonst x) + (FFKonst y)               = FFKonst (x+y)
+  (FFElem f p) + (FFKonst x)              = FFElem (modByP (f+P [(0,x)]) p) p
+  -- ...
+  (FFElem f p) + (FFElem g q) | p==q       = FFElem (modByP (f+g) p) p
+                              | otherwise = error "Not the same mod"
+  (FFElem f p) * (FFElem g q) | p==q       = FFElem (modByP (f*g) p) p
+                              | otherwise = error "Not the same mod"
+  negate (FFElem f p)                     = FFElem (negate f) p
+  abs _                                   = error "Prelude.Num.abs: inappropriate abstraction"
+  signum _                                = error "Prelude.Num.signum: inappropriate abstraction"
+
+instance (Eq a, Fractional a) => Fractional (FFElem a) where
+  recip (FFElem f p)  =  FFElem s p
+    where (d,s,t) = eekP f p
+  fromRational _ = error "inappropriate abstraction"
+
+instance (FiniteField a) => FiniteField (FFElem a) where
+  one  = FFKonst one
+  zero = FFKonst zero
+  elems = undefined
+  units = undefined
 
 --------------------------------------------------------------------------------
 --  Beispiel
