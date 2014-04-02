70b75b0571a41958bdfd3da5296aec73a12c72ca
maximilianhuber <hubi@by-hubi.de>
2014-04-02 16:41:37 +0200

    done latex

--- src/Projekt/Core/LatexShow.hs
+++ src/Projekt/Core/LatexShow.hs
@@ -8,33 +8,34 @@
 --------------------------------------------------------------------------------
 module Projekt.Core.LatexShow
   ( LatexShow(..)
-  , latexRender
+  , latexRender, latexRenderRaw
   ) where
 import System.Process
 
 class LatexShow a where
   latexShow :: a -> String
 
-latexRender :: (LatexShow a) => a -> IO String
-latexRender x = readProcess "bash/renderLatexSnipet.sh" [latexShow x] []
+--------------------------------------------------------------------------------
+--  Use latex and divpng to render Latex snippets
 
-{-
-#!/bin/sh
+latexRender :: (LatexShow a) => a -> IO ()
+latexRender = latexRenderRaw . latexShow
 
-latex -halt-on-error -output-directory /tmp \
- '\documentclass[12pt]{article}' \
- '\pagestyle{empty}' \
- '\begin{document}' \
- '\[' \
- "$@" \
- '\]' \
- '\end{document}' > /dev/null
-
-dvipng -gamma 2 -z 9 -T tight \
-  -bg Transparent \
-  -o /tmp/snipet.png /tmp/article.dvi > /dev/null
-
-dvipng -gamma 2 -z 9 -T tight \
-  -bg White \
-  -o /tmp/snipet-w.png /tmp/article.dvi > /dev/null
- -}
+latexRenderRaw :: String -> IO ()
+latexRenderRaw x = do createProcess (shell cmd)
+                      return ()
+  where cmd = "latex -halt-on-error -output-directory /tmp "
+               ++ "\"\\documentclass[12pt]{article}"
+               ++ "\\pagestyle{empty}"
+               ++ "\\begin{document}"
+               ++ "\\["
+               ++ x
+               ++ "\\]"
+               ++ "\\end{document}\""
+               ++ " > /dev/null"
+               ++ " ; "
+               ++ "dvipng " 
+               ++ "-gamma 2 -z 9 -T tight "
+               ++ "-bg Transparent "
+               ++ "-o /tmp/snipet.png /tmp/article.dvi"
+               ++ " > /dev/null"
