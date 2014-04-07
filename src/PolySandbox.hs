3c33c81f5b53c3d87404e4465bf723d758a2fa5c
maximilianhuber <hubi@by-hubi.de>
2014-04-07 11:26:47 +0200

    edit tests

--- src/PolySandbox.hs
+++ src/PolySandbox.hs
@@ -35,26 +35,33 @@ exmpPolyMod = aggP $ P [(10,5::Z101),(10,4::Z101),(3,2::Z101),(0,5::Z101)]
 exmpPolyMod' = aggP $ P [(8,5::Z101),(9,4::Z101),(3,2::Z101),(0,5::Z101)]
 
 unEekP (d,s,t) a b = d == s*a + t*b
+unDivP (q,r) a b = a == q * b + r
 
 --------------------------------------------------------------------------------
+testSize = 10
+
+subroutine l= do
+  it "test divP (x*1=x)" $ mapM_
+    (\ x -> multP x (P[(0,1)]) == x `shouldBe` True)
+    l
+  it "test divP (x/x=1)" $ mapM_
+    (\ x -> divP x x == (P[(0,1)], P[]) `shouldBe` True)
+    l
+  it "test divP generally" $ mapM_
+    (\ (x,y) -> unDivP (divP x y) x y `shouldBe` True)
+    (zip (take testSize l) (drop testSize l))
+  it "test eekP" $ mapM_
+    (\ (x, y) -> unEekP (eekP x y) x y `shouldBe` True)
+    (zip (take testSize l) (drop testSize l))
+
 main :: IO ()
 main = do
-  let testSize = 10
   list <- rndSelect (getAllP (elems undefined ::[Z5]) 4) (2*testSize)
   uList <- rndSelect (getAllP (elems u) 4) (2*testSize)
-  vList <- rndSelect (getAllP (elems v) 3) (2*testSize)
+  vList <- rndSelect (getAllP (elems v) 2) (2*testSize)
   wList <- rndSelect (getAllP (elems w) 4) (2*testSize)
-  hspec $
-    describe "Projekt.Core.Polynomials" $ do
-      it "test eekP over Z101" $ mapM_
-        (\ (x, y) -> unEekP (eekP x y) x y `shouldBe` True)
-        (zip (take testSize list) (drop testSize list))
-      it "test eekP over u" $ mapM_
-        (\ (x, y) -> unEekP (eekP x y) x y `shouldBe` True)
-        (zip (take testSize uList) (drop testSize uList))
-      it "test eekP over v" $ mapM_
-        (\ (x, y) -> unEekP (eekP x y) x y `shouldBe` True)
-        (zip (take testSize vList) (drop testSize vList))
-      it "test eekP over w" $ mapM_
-        (\ (x, y) -> unEekP (eekP x y) x y `shouldBe` True)
-        (zip (take testSize wList) (drop testSize wList))
+  hspec $ do
+    describe "Projekt.Core.Polynomials @Z101" $ subroutine list
+    describe "Projekt.Core.Polynomials @u" $ subroutine uList
+    describe "Projekt.Core.Polynomials @v" $ subroutine vList
+    describe "Projekt.Core.Polynomials @w" $ subroutine wList
