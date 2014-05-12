626f257d85dcc25e5fd1d95f2af47714c3379fbe
maximilianhuber <hubi@by-hubi.de>
2014-05-12 15:15:04 +0200

    fertig toArray

--- src/Projekt/Core/Matrix.hs
+++ src/Projekt/Core/Matrix.hs
@@ -1,13 +1,15 @@
 module Projekt.Core.Matrix
   ( Matrix (..), genDiagM, fromListsM
-  -- tests
-  , isQuadraticM
   -- getter
   , atM, getNumRowsM, getNumColsM
+  -- tests
+  , isQuadraticM
   -- operations
   , transposeM
+  -- Funktionen
+  , swapRowsM, swapColsM, subM
   -- linear algebra
-  , detLapM
+  , detLapM, detM
   )
   where
 import Data.List
@@ -20,6 +22,7 @@ import Debug.Trace
 --------------------------------------------------------------------------------
 --  Data Definition
 
+-- Zeile x Spalte
 data Matrix a = M {unM :: Array (Int, Int) a} | Mdiag a
 
 --------------------------------------------------------------------------------
@@ -30,13 +33,12 @@ fillList ls n m = ls ++ [(idx,0) | idx <- getAllIdxsExcept n m idxs]
         getAllIdxsExcept n m idxs = [idx | idx <- [(i,j) | i <- [1..n], j <- [1..m]]
                                          , idx `notElem` idxs]
 
-
 isQuadraticM :: Matrix a -> Bool
 sQuadraticM (Mdiag a) = True
 isQuadraticM (M m) = uncurry (==) b
   where b = snd $ bounds m
 
-genDiagM :: (Num a) => a -> Int -> Matrix a
+genDiagM :: Num a => a -> Int -> Matrix a
 genDiagM x n = M $ array ((1,1),(n,n)) $ fillList [((i,i),x) | i <- [1..n]] n n
 
 fromListsM :: [[a]] -> Matrix a
@@ -44,6 +46,18 @@ fromListsM ess = M $ array ((1,1),(k,l)) [((i,j),ess!!(j-1)!!(i-1)) | i <- [1..k
                                                                     , j <- [1..l]]
   where k = length $ head ess
         l = length ess
+
+--------------------------------------------------------------------------------
+--  Getter
+
+atM :: Matrix a -> Int -> Int -> a
+atM (M m) row col = m!(row,col)
+
+getNumRowsM :: Matrix a -> Int
+getNumRowsM (M m) = fst $ snd $ bounds m
+
+getNumColsM :: Matrix a -> Int
+getNumColsM (M m) = snd $ snd $ bounds m
 
 --------------------------------------------------------------------------------
 --  Instanzen
@@ -55,15 +69,12 @@ instance Show a => Show (Matrix a) where
                              | i <- [1..(fst b)]]
     where b = snd $ bounds m
 
-{-
 instance (ShowTex a,Eq a) => ShowTex (Matrix a) where
-  showTex (M [])    = ""
-  showTex (M [[]])  = ""
   showTex (Mdiag a) = "[" ++ showTex a ++ "]"
-  showTex (M m)     = "\\begin{pmatrix}" ++ showTex' m ++ "\\end{pmatrix}"
-    where showTex'         = concatMap ((++ "\\\\") . showTex'')
-          showTex'' (x:xs) = (showTex x ++) $ concatMap (('&':) . showTex) xs
- -}
+  showTex (M m)     = "\\begin{pmatrix}" ++ showTex' ++ "\\end{pmatrix}"
+    where (k,l)    = snd $ bounds m
+          showTex' = concatMap (++ "\\\\")
+            [concatMap ('&':) [showTex (m!(i,j)) | j <- [1..l]] | i <- [1..k]]
 
 instance (Eq a, Num a) => Eq (Matrix a) where
   Mdiag x == m = genDiagM x (getNumRowsM m) == m
@@ -101,57 +112,47 @@ multM (M m)     (M n)     | k' == l    = M $ array ((1,1),(k,l'))
   where ((_,_),(k,l))   = bounds m
         ((_,_),(k',l')) = bounds n
 
-
-{-
--- 'concat' matrices horizontally
-(<|>) :: Matrix a -> Matrix a -> Matrix a
-(<|>) (M m1) (M m2) | test      = M [ m1 !! i ++ m2 !! i | i <- [0..(n-1)] ]
-                    | otherwise = error "not same row count"
-  where test = getNumRowsM (M m1) == getNumRowsM (M m2)
-        n    = getNumRowsM (M m1)
-
--- 'concat' matrices vertically
-(<-->) :: Matrix a -> Matrix a -> Matrix a
-(<-->) (M m1) (M m2) | test      = M $ m1 ++ m2
-                   | otherwise = error "not same column count"
-  where
-    test = getNumColsM (M m1) == getNumColsM (M m2)
- -}
-
---------------------------------------------------------------------------------
---  Getter
-
-atM :: Matrix a -> Int -> Int -> a
-atM (M m) row col = m!(row,col)
-
-getNumRowsM :: Matrix a -> Int
-getNumRowsM (M m) = fst $ snd $ bounds m
-
-getNumColsM :: Matrix a -> Int
-getNumColsM (M m) = snd $ snd $ bounds m
-
-{-
--- get column
-(!|) :: Matrix a -> Int -> [a]
-(!|) (M m) n  = [m !! i !! n | i <- [0..r-1]]
-  where r = getNumRowsM $ M m
-
--- get row
-(!-) :: Matrix a -> Int -> [a]
-(M m) !- n  = m !! n
-
--- get rows
-(!!-) :: Matrix a -> [Int] -> Matrix a
-(M m) !!- ns  = M [m !! i | i <- ns]
-
--- get columns
-(!!|) :: Matrix a -> [Int] -> Matrix a
-(!!|) (M m) ns  = M [[m !! i !! j | j <- ns] | i <- [0..r-1]]
-  where r = getNumRowsM $ M m
--}
-
 --------------------------------------------------------------------------------
 --  Funktionen auf Matrizen
+
+-- |Gibt zu einer Matrix eine Untermatrix zurrück
+-- Input:
+--      (k0,l0) : erste übernommene Spalte und Zeile
+--      (k1,l1) : letzte übernommene Spalte und Zeile
+--      m       : eingabe Matrix
+subM :: Num a => (Int,Int) -> (Int,Int) -> Matrix a -> Matrix a
+subM (k0,l0) (k1,l1) (Mdiag x) = subM (k0,l0) (k1,l1) $ genDiagM x $ max k1 l1
+subM (k0,l0) (k1,l1) (M m)     = M $ subArr (k0,l0) (k1,l1) m
+
+subArr :: Num a => (Int,Int) -> (Int,Int) -> Array (Int, Int) a
+                                                          -> Array (Int, Int) a
+subArr (k0,l0) (k1,l1) m =  array ((1,1),(k,l))
+    [ ((i-k0+1,j-l0+1) , m!(i,j)) | i <- [k0..k1] , j <- [l0..l1]]
+  where (k,l) = (k1-k0+1,l1-l0+1)
+
+swapRowsM :: Num a => Int -> Int -> Matrix a -> Matrix a
+swapRowsM _ _ (Mdiag x) = error "Not enougth information given"
+swapRowsM k0 k1 (M m)   = M $ swapRowsArr k0 k1 m
+
+swapRowsArr :: Num a => Int -> Int -> Array (Int, Int) a -> Array (Int, Int) a
+swapRowsArr k0 k1 m = array ((1,1),(k,l))
+    [ ((swp i,j) , m!(i,j)) | i <- [1..k] , j <- [1..l]]
+  where (k,l) = snd $ bounds m
+        swp i | i == k0    = k1
+              | i == k1    = k0
+              | otherwise = i
+
+swapColsM :: Num a => Int -> Int -> Matrix a -> Matrix a
+swapColsM _ _ (Mdiag x) = error "Not enougth information given"
+swapColsM l0 l1 (M m) = M $ swapColsArr l0 l1 m
+
+swapColsArr :: Num a => Int -> Int -> Array (Int, Int) a -> Array (Int, Int) a
+swapColsArr l0 l1 m = array ((1,1),(k,l))
+    [ ((i,swp j) , m!(i,j)) | i <- [1..k] , j <- [1..l]]
+  where (k,l) = snd $ bounds m
+        swp j | j == l0    = l1
+              | j == l1    = l0
+              | otherwise = j
 
 -- |Transponiere eine Matrix
 transposeM :: Matrix a -> Matrix a
@@ -176,64 +177,32 @@ detLapM' m | b == (1,1) = m!(1,1)
             ++ [((i',j'),m!(i'+1,j'+1)) | i' <- [i..(fst b - 1)]
                                        , j' <- [1..(snd b - 1)]]
 
-
-{-
--- |Vertausche zwei Zeilen einer Matrix
-swapRowsM :: Matrix a -> Int -> Int -> Matrix a
-swapRowsM m r1 r2 = M $ swapItems (unM m) r1 r2
-
--- |Vertausche zwei Spalten einer Matrix
-swapColsM :: Matrix a -> Int -> Int -> Matrix a
-swapColsM m r1 r2 = M $ map (\x -> swapItems x r1 r2) $ unM m
-
-swapItems :: [a] -> Int -> Int -> [a]
-swapItems ls r1 r2 = [get k x | (x,k) <- zip ls [0..]]
-    where get k x | k == r1 = ls !! r1
-                  | k == r1 = ls !! r2
-                  | otherwise = x
--}
-
-{-
-triangularM :: (Eq a, Fractional a) => Matrix a -> Matrix a
-triangularM (Mdiag m) = Mdiag m
-triangularM (M m)     = M $ triangular' 0 m
-  where triangular' n [] = []
-        triangular' n m' = row : triangular' (n+1) rows'
-          where (row:rows) = pivotAndSwap n m'
-                rows'      = map eval rows
-                eval rs    | (rs !! n) == 0 = rs
-                           | otherwise     = zipWith (-) (map (*c) rs) row
-                  where c = (row !! n) / (rs !! n)
-
-        pivotAndSwap :: (Eq a, Num a) => Int -> [[a]] -> [[a]]
-        pivotAndSwap n (row:rows)
-          | (row !! n) /= 0
-            || null rows
-            || all (==True) (map (all (==0)) rows) = row:rows
-          | otherwise                            = pivotAndSwap n (rows ++ [row])
-
-{-fulltriangularM :: (Eq a, Fractional a) => Matrix a -> Matrix a-}
-{-fulltriangularM  = backtrackM . triangularM-}
-
--- backtrack an upper triangular Matrix a
-{-backtrackM :: (Eq a, Fractional a) => Matrix a -> Matrix a-}
-{-backtrackM (M m)  = M $ backtrack' m-}
-  {-where backtrack' [] = []-}
-        {-backtrack' as = (backtrack' (init $ eval as)) ++ (last eval as)-}
-          {-where eval as = undefined -- | f == Nothing   =  as-}
-                        {-| otherwise     =  undefined map (zipWith (-) $ map (/f) l) as-}
-              {-where l = last as-}
-                {-f = find (/=0) l-}
-
-detM :: (Eq a, Fractional a) => Matrix a -> a
+-- |Berechne die Determinante effektiver als detLapM aber braucht Fractional
+detM :: (Eq a, Num a, Fractional a) => Matrix a -> a
 detM (Mdiag 0) = 0
 detM (Mdiag 1) = 1
 detM (Mdiag _) = error "Not enougth information given"
-detM m         | isQuadraticM m =
-                  product [atM (triangularM m) i i | i <- [0..(getNumRowsM m -1)]]
+detM m         | isQuadraticM m = detArr $ unM m
                | otherwise      = error "Matrix not quadratic"
+  where -- |detM auf Array ebene
+        detArr :: (Eq a, Num a, Fractional a) => Array (Int, Int) a -> a
+        detArr m | k == 1       = m!(1,1)
+                 | m!(1,1) == 0 = - detArrPivot m
+                 | otherwise = (m!(1,1) *) $ detArr $ subArr (2,2) (k,l) $
+                   detArrElim m
+          where (k,l) = snd $ bounds m
 
-{-invM :: (Eq a, Fractional a) => Matrix a -> Matrix a-}
-  {-invM m | !isQuadraticM m  = error "Matrix not quadratic"-}
-         {-| -}
- -}
+        -- |Sucht ein Pivot Element und vertauscht wenn nötig
+        detArrPivot :: (Eq a, Num a, Fractional a) => Array (Int, Int) a -> a
+        detArrPivot m | null lst  = 0
+                      | otherwise = detArr $ swapColsArr 1 (minimum lst) m
+          where (k,l) = snd $ bounds m
+                lst   = [i | i <- [1..k] , m!(i,1) /= 0]
+
+        -- |Zieht die erste Zeile passend von allen anderen ab
+        detArrElim :: (Eq a, Num a, Fractional a) => Array (Int, Int) a
+                                                          -> Array (Int, Int) a
+        detArrElim m = array ((1,1),(k,l)) $
+          concat [[((i,j), m!(i,j) - m!(i,1) / m!(1,1) * m!(1,j)) | j <- [1..l]]
+            | i <- [1..k]]
+              where (k,l) = snd $ bounds m
