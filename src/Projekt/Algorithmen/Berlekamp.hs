01af968d0b3d48c4b055074f38d1b58df1c9ac49
hackenbergstefan <hackenberg.stefan@t-online.de>
2014-05-16 18:56:13 +0200

    .

--- src/Projekt/Algorithmen/Berlekamp.hs
+++ src/Projekt/Algorithmen/Berlekamp.hs
@@ -7,12 +7,12 @@ import Projekt.Core.Matrix
 import Debug.Trace
 
 
-{-
+
 berlekampMatrix :: (Show a, Fractional a, Num a, FiniteField a) => Polynom a -> Matrix a
-berlekampMatrix f = transposeM $ M [red i | i <- [0..(n-1)] ]
+berlekampMatrix f = kernelM $ transposeM $ 
+                        fromListsM [red i | i <- [0..(n-1)]] - genDiagM 1 n 
   where n     = fromJust $ degP f
         q     = elemCount a
         a     = prodOfCoeffsP f
         red i = take n $ (unP (snd (divP (fromMonomialsP [(i*q,1)]) f))
                               ++ [0*a | i <- [0..]] )
- -}
