c0424e577a28ed23963fb5bd5a70c148002b254f
maximilianhuber <hubi@by-hubi.de>
2014-05-18 12:32:29 +0200

    add stolen things

--- src/Stolen/Cache.hs
+++ src/Stolen/Cache.hs
@@ -19,30 +19,30 @@ newtype Cache k v = Cache (Chan (CacheMsg k v))
 
 cacheThread :: (Ord k) => Chan (CacheMsg k v) -> IO ()
 cacheThread chan = next Map.empty
-	where
-	next m = readChan chan >>= go m
+  where
+  next m = readChan chan >>= go m
 
-	go m (CacheMsg k io reply) = case Map.lookup k m of
-		Just v -> putMVar reply v >> next m
-		Nothing -> do
-			v <- io
-			putMVar reply v
-			next (Map.insert k v m)
+  go m (CacheMsg k io reply) = case Map.lookup k m of
+    Just v -> putMVar reply v >> next m
+    Nothing -> do
+      v <- io
+      putMVar reply v
+      next (Map.insert k v m)
 
 -- | Create a new cache
 newCache :: (Ord k) => IO (Cache k v)
 newCache = do
-	chan <- newChan
-	-- This cache thread never terminates, so this is for a process-life cache
-	-- That would be easy to change, but I won't bother here
-	void $ forkIO (cacheThread chan)
-	return (Cache chan)
+  chan <- newChan
+  -- This cache thread never terminates, so this is for a process-life cache
+  -- That would be easy to change, but I won't bother here
+  void $ forkIO (cacheThread chan)
+  return (Cache chan)
 
 syncCall :: Chan a -> (MVar r -> a) -> IO r
 syncCall chan msg = do
-	r <- newEmptyMVar
-	writeChan chan (msg r)
-	takeMVar r
+  r <- newEmptyMVar
+  writeChan chan (msg r)
+  takeMVar r
 
 -- | Get a value from the cache, or compute it and cache it
 fromCache :: Cache k v -> k -> IO v -> IO v
