58c7c6f2bdf3cdbf07acd3169b9e31639c384a6a
maximilianhuber <hubi@by-hubi.de>
2014-05-20 16:42:20 +0200

    small folder change

--- src/bash/runProfiling.sh
+++ src/bash/runProfiling.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
-mkdir -p ${DIR}outP
+mkdir -p ${DIR}out
+mkdir -p ${DIR}out/Profiling
 
 ghc \
-  -outputdir ${DIR}outP \
-  -o ${DIR}outP/AlgProfiling \
+  -outputdir ${DIR}out/Profiling \
+  -o ${DIR}out/Profiling/AlgProfiling \
   -fforce-recomp \
   -prof \
   -fprof-auto -rtsopts \
     ${DIR}AlgProfiling.hs
 
 if [ $? -eq 0 ]; then
-  ${DIR}outP/AlgProfiling +RTS -p
+  ${DIR}out/Profiling/AlgProfiling +RTS -p
 fi
 
 # -fforce-recomp, -O2 and -fllvm are from:
