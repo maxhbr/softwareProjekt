7195d713efdd06e345ca751d226105c425376823
maximilianhuber <hubi@by-hubi.de>
2014-05-21 19:00:56 +0200

    update

--- src/Projekt/Algorithmen/Berlekamp.hs
+++ src/Projekt/Algorithmen/Berlekamp.hs
@@ -1,58 +1,86 @@
-
-module Projekt.Algorithmen.Berlekamp where
+--------------------------------------------------------------------------------
+-- |
+-- Module      : Project.Algorithmen.Berlekamp
+-- Note        : Implementiert eine Berlekamp Faktorisierung
+--
+-- Funktioniert nur auf Quadratfreien Polynomen
+--
+--------------------------------------------------------------------------------
+module Projekt.Algorithmen.Berlekamp
+  ( berlekamp, appBerlekamp, sffAndBerlekamp
+  -- Algorithmus
+  , berlekampBasis
+  , berlekampFactor
+  )where
 import Data.Maybe
 import Data.List
 import Projekt.Core.FiniteFields
 import Projekt.Core.Polynomials
 import Projekt.Core.Matrix
+import Projekt.Core.Factorization
 import Debug.Trace
-import Data.MemoTrie
+import Projekt.Algorithmen.SFreeFactorization
 
+--------------------------------------------------------------------------------
+--  Wrapper
 
-berlekampFactor = undefined
+berlekamp :: (Show a ,FiniteField a, Num a, Fractional a) => Polynom a -> [(Int,Polynom a)]
+berlekamp = appFact berlekampFactor . obviousFactor
 
-{-
+appBerlekamp :: (Show a, FiniteField a, Num a, Fractional a) => [(Int,Polynom a)] -> [(Int,Polynom a)]
+appBerlekamp = appFact berlekamp
+
+-- |Faktorisiert ein Polynom f über einem endlichen Körper
+-- Benutzt wird dazu die Quadratfreie Faktorisierung mit anschließendem
+-- Berlekamp
+sffAndBerlekamp :: (Show a, Fractional a, Num a, FiniteField a)
+                                              => Polynom a -> [(Int,Polynom a)]
+sffAndBerlekamp f = appBerlekamp $ sff f
+
+--------------------------------------------------------------------------------
+--  Algorithmus
+
 -- |Berechnet eine Basis des Berlekampraums zu f,
---  d.h. gibt eine Matrix zurück, deren Zeilen gerade den Berlekampraum 
+--  d.h. gibt eine Matrix zurück, deren Zeilen gerade den Berlekampraum
 --  aufspannen bzgl der kanonischen Basis { 1, x, x², x³, ... }
-berlekampBasis :: (Show a, HasTrie a, Fractional a, Num a, FiniteField a) 
-                                                     => Polynom a -> Matrix a
-berlekampBasis f = transposeM $ kernelM $ transposeM $ 
-                        fromListsM [red i | i <- [0..(n-1)]] - genDiagM 1 n 
+berlekampBasis :: (Show a, Fractional a, Num a, FiniteField a)
+                                                       => Polynom a -> Matrix a
+berlekampBasis f = transposeM $ kernelM $ transposeM $
+                        fromListsM [red i | i <- [0..(n-1)]] - genDiagM 1 n
   where n     = fromJust $ degP f
         q     = elemCount a
         a     = prodOfCoeffsP f
-        red i = take n $ (unP (snd (divP (fromMonomialsP [(i*q,1)]) f))
+        red i = take n (unP (snd (divP (fromMonomialsP [(i*q,1)]) f))
                               ++ [0*a | i <- [0..]] )
-
 
 -- |Faktorisiert ein Polynom f über einem endlichen Körper
 --  Voraussetzungen: f ist quadratfrei
 --  Ausgabe: Liste von irreduziblen, pw teilerfremden Polynomen
-berlekampFactor :: (Show a, HasTrie a, Fractional a, Num a, FiniteField a)
-                                                  => Polynom a -> [Polynom a]
+berlekampFactor :: (Show a, Fractional a, Num a, FiniteField a)
+                                              => Polynom a -> [(Int,Polynom a)]
+berlekampFactor (P[]) = [(1,P[])]
+berlekampFactor (P[m]) = [(1,P[m])]
+berlekampFactor (P[m0,m1]) = [(1,P[m0,m1])]
 berlekampFactor f = berlekampFactor' f m
   where m = berlekampBasis f
-        berlekampFactor' :: (Num a, Fractional a, FiniteField a) 
-                                      => Polynom a -> Matrix a -> [Polynom a]
-        berlekampFactor' f m | (fromJust $ degP f) <= 1 = [f]
-                             | getNumRowsM m == 1       = [f]
-                             | otherwise               = (berlekampFactor' g n)
-                                                    ++ (berlekampFactor' g' n')
-          where g  = head $ filter (\x -> x /= 1 && x /= f)
-                    [ggTP f (h - (P [s])) | s <- (elems ((unP f)!!0))]
+        berlekampFactor' :: (Show a, Num a, Fractional a, FiniteField a)
+                                      => Polynom a -> Matrix a -> [(Int,Polynom a)]
+        berlekampFactor' f m | uDegP f <= 1       = [(1,f)]
+                             | getNumRowsM m == 1 = [(1,f)]
+                             | otherwise         = berlekampFactor' g n
+                                                 ++ berlekampFactor' g' n'
+          where g  = head [x | x <- [ggTP f (h - P [s]) | s <- elems (getReprP f)]
+                             , x /= 1]
                 g' = f @/ g
                 h  = P $ getRowM m 2
                 n  = newKer m g
                 n' = newKer m g'
                 newKer m g  = fromListsM $ take r m'
                   where (k,l) = boundsM m
-                        m'    = toListsM $ echelonM $ fromListsM
-                                  [ take l $ unP $ modByP (P (getRowM m i)) g
-                                                                | i <- [1..k]] 
-                        r     = k-1- (fromMaybe (-1) 
-                                      $ findIndex (\x -> (all (\y -> y==0) x)) 
+                        m'    = toListsM $ echelonM $ fromListsM $ take r'' m''
+                        m''   = [take l $ unP $ modByP (P (getRowM m i)) g
+                                                                | i <- [1..k]]
+                        r     = k-1- fromMaybe (-1) (findIndex (all (==0))
                                                         $ reverse m')
-
-    
- -}
+                        r''   = k-1- fromMaybe (-1) (findIndex (all (==0))
+                                                        $ reverse m'')
--- src/Projekt/Algorithmen/SFreeFactorization.hs
+++ src/Projekt/Algorithmen/SFreeFactorization.hs
@@ -1,13 +1,30 @@
+--------------------------------------------------------------------------------
+-- | 
+-- Module      : Project.Algorithmen.SFreeFactorization
+-- Note        : Implementiert eine Quadratfreie Faktorisierung
+-- 
+-- 
+-- 
+--------------------------------------------------------------------------------
 module Projekt.Algorithmen.SFreeFactorization
-  (sff
+  (sff, appSff
+  , sffFactor
   ) where
 import Projekt.Core.FiniteFields
 import Projekt.Core.Polynomials
 import Projekt.Core.Factorization
 
-import Data.MemoTrie
+--------------------------------------------------------------------------------
+--  Wrapper
 
-import Debug.Trace
+sff :: (FiniteField a, Num a, Fractional a) => Polynom a -> [(Int,Polynom a)]
+sff = appFact sffFactor . obviousFactor
+
+appSff :: (FiniteField a, Num a, Fractional a) => [(Int,Polynom a)] -> [(Int,Polynom a)]
+appSff = appFact sff
+
+--------------------------------------------------------------------------------
+--  Algorithmus
 
 {-
  - from: http://en.wikipedia.org/wiki/Factorization_of_polynomials_over_finite_fields
@@ -41,47 +58,24 @@ import Debug.Trace
        Seite 345(355)
  -}
 
-sff :: (FiniteField a, HasTrie a, Num a, Fractional a) => Polynom a -> [(Int,Polynom a)]
-sff f | df /= 0 && c /= 1 = r ++ map (\(n,x) -> (n*p,x)) (sff $ charRootP c)
+sffFactor :: (FiniteField a, Num a, Fractional a) => Polynom a -> [(Int,Polynom a)]
+sffFactor (P[]) = [(1,P[])]
+sffFactor (P[m]) = [(1,P[m])]
+sffFactor (P[m0,m1]) = [(1,P[m0,m1])]
+sffFactor f | df /= 0 && c /= 1 = r ++ map (\(n,x) -> (n*p,x)) (sffFactor $ charRootP c)
       | df /= 0 && c == 1 = r
-      | otherwise       = map (\(n,x) -> (n*p,x)) $ sff $ charRootP f
+      | otherwise       = map (\(n,x) -> (n*p,x)) $ sffFactor $ charRootP f
   where df         = deriveP f
         c'         = ggTP f df
-        p          = charakteristik $ prodOfCoeffsP f
-        (r,c)      = sff' 1 (f @/ c') c'
-        sff' i w c | w == 1      = ([], c)
+        p          = charOfP f
+        (r,c)      = sffFactor' 1 (f @/ c') c'
+        sffFactor' i w c | w == 1      = ([], c)
                    | z /= P[one] = ((i,z) : r, c')
                    | otherwise  = (r,c')
           where y      = ggTP w c
                 z      = w @/ y
-                (r,c') = sff' (i+1) y (c @/ y)
+                (r,c') = sffFactor' (i+1) y (c @/ y)
 
-{-
--- |Noch ein Versuch, zum verständnis:
--- TODO: was ist hier falsch
-sfff :: (FiniteField a, Num a, Fractional a) => Polynom a -> [(Int,Polynom a)]
-sfff (P[]) = []
-sfff f     | uDegP f == 0 = [(1,f)]
-           | df /= 0      = sfff'
-           | otherwise   = map (\(n,x) -> (n*p,x)) $ sfff $ charRootP f
-  where df    = deriveP f
-        p     = charakteristik $ prodOfCoeffsP f
-
-        -- |Hier kann davon ausgegangen werden, dass die Ableitung von f nicht
-        -- verschwindet, deshalb kann man hier den klassischen Algorithmus
-        -- anwenden
-        --      im Fall g==1 ist das Polynom f bereits quadratfrei
-        --      sonst ist: alles was in g enthalten ist  mindestens in
-        --                   quadratischer Ordnung in f (jetzt eine Ordnun
-        --                   niedriger)
-        --                 das h sind die teile, die in f nur noch in Ordnun 1
-        --                   vorkommen und somit für den nächsten schritt
-        --                   rausfliegen
-        sfff' | g == 1     = [(1,f)]
-              | otherwise = (1,h) : map (\(n,x) -> (n+1,x)) (sfff g)
-          where g = ggTP f df
-                h = fst $ divP f g
- -}
 
 --------------------------------------------------------------------------------
 --  Beispiele
@@ -97,12 +91,3 @@ sqf=[(1,P[1::F3,1])
     ,(2,P[1])
     ,(3,P[1::F3,0,1])
     ,(4,P[2::F3,1])]
-
-e4f2Mipo = P[1::F2,1::F2,0,0,1::F2]
-failF = P $ listFFElem e4f2Mipo [ P[0::F2,0,1,1]
-                                , 1
-                                , P[1::F2,1,1]
-                                , P[0::F2,1]
-                                , P[1::F2,1,0,1] ]
-
-ff = P[2::F3,1]*P[1,0,1]
