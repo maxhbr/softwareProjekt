ec0cc9b37df3a30ea44627846aa47b0d7b5b4b06
maximilianhuber <mail@maximilian-huber.de>
2014-06-21 14:42:29 +0200

    marginal change

--- bash/initSandbox.sh
+++ bash/initSandbox.sh
@@ -2,8 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 if [ ! -d "${DIR}/.cabal-sandbox/ " ]; then
+  pushd $DIR
   cabal sandbox init \
     && cabal install --only-dependencies -j \
     && cabal install -j \
     && cabal configure --enable-benchmarks --enable-tests
+  popd
 fi
