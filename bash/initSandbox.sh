688367764cd739e3a370ba7acee54f241adc475d
maximilianhuber <mail@maximilian-huber.de>
2014-06-25 13:39:36 +0200

    change initSandbox.sh

--- bash/initSandbox.sh
+++ bash/initSandbox.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+pushd $DIR
 #if [ ! -d "${DIR}/.cabal-sandbox" ]; then
 if [ ! -f "${DIR}/cabal.sandbox.config" ]; then
-  pushd $DIR
   cabal sandbox init \
     && cabal install --only-dependencies -j \
     && cabal configure \
     && cabal build
     #&& cabal install -j \
-  popd
 fi
 
 cabal configure \
   && cabal sdist
+popd
