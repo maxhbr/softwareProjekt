751e0f126ef27db58bc83c6554bd34fae7b03c08
maximilianhuber <mail@maximilian-huber.de>
2014-06-25 09:52:08 +0200

    repair initSandbox

--- bash/initSandbox.sh
+++ bash/initSandbox.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
-if [ ! -d "${DIR}/.cabal-sandbox/ " ]; then
+#if [ ! -d "${DIR}/.cabal-sandbox" ]; then
+if [ ! -f "${DIR}/cabal.sandbox.config" ]; then
   pushd $DIR
   cabal sandbox init \
     && cabal install --only-dependencies -j \
