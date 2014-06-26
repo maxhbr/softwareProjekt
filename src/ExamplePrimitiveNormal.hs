d500f0ec72891eb8cfc89fd18a61c310709a4929
maximilianhuber <mail@maximilian-huber.de>
2014-06-26 21:58:26 +0200

    change exmpPrimNorm a little bit

--- src/ExamplePrimitiveNormal.hs
+++ src/ExamplePrimitiveNormal.hs
@@ -1,17 +1,25 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Mail where
 import GalFld.GalFld
 import GalFld.SpecialPolys
+import System.CPUTime
 
-pf = 1::F2
+$(genPrimeField 2 "PF")
+
+pf = 1::PF
 p = charakteristik pf
 
-main = do
-    mapM_ (\n -> putStrLn $ "Es gibt " ++ show (countPrimNorm n) 
-            ++ " primitive und normale Elemente in F"
-            ++show p++"^"++show n++" über F"++show p) 
-          [2..]
-      where countPrimNorm n = uDegP ggT
-              where cyP = cyclotomicPoly (p^n-1) pf
-                    piP = piPoly $ pTupUnsave [(n,pf),(0,-1)]
-                    ggT = ggTP cyP piP 
-                    fac = factorP $ ggT
-                    deg = uDegP $ snd $ head fac
+main = mapM_ (\n -> do
+  startTime <- getCPUTime
+  putStr $ "Es gibt " ++ show (countPrimNorm n)
+        ++ " primitive und normale Elemente in F"
+        ++show p++"^"++show n++" über F"++show p
+  finishTime <- getCPUTime
+  putStrLn $ " ("
+    ++ show (fromIntegral (finishTime - startTime) / 1000000000000) ++ "s)") [2..]
+    where countPrimNorm n = uDegP $ ggTP cyP piP
+            where cyP = cyclotomicPoly (p^n-1) pf
+                  piP = piPoly $ pTupUnsave [(n,pf),(0,-1)]
+                  --fac = factorP ggT
+                  --deg = uDegP $ snd $ head fac
