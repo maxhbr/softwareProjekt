b9d3751a43b9dd9a98118938eea24b93bc1aa910
hackenbergstefan <hackenberg.stefan@t-online.de>
2014-07-16 09:01:17 +0200

    .

--- tex/compile.sh
+++ tex/compile.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-/usr/bin/latexmk -lualatex --shell-escape -synctex=1 -pvc main.tex
+/usr/bin/latexmk -lualatex --shell-escape -synctex=1 -pvc \
+  -outdir=/tmp/latexmk_files/ -auxdir=/tmp/latexmk_files/ \
+  main.tex
 
